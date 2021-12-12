65bc54a4e8f38296537175ea41b14752a51465cc
Edwin Caceres <administrador@MacBook-Pro-de-Administrador.local>
2021-12-12 11:33:46 -0500

    Learning more about child process

--- childprocess.js
+++ childprocess.js
@@ -1,22 +1,40 @@
 const {exec,spawn}=require('child_process');
+const child= spawn('pwd');
 
-let myProcess=spawn('ls',['-la']);
+
+let myProcess=spawn('ls',['-la']); //Other commands are added inside an array
 console.log(myProcess)
 
 myProcess.stdout.on('data',function(data){
 		console.log(data.toString())
 })
 
-myProcess.on('exit',function(){
-	console.log('The process has finished')
+myProcess.stderr.on('data',function(data){ //Only will execute is child process exists with code 1, which indicates an error
+		console.log(`This is the sterr: ${data}`)
 })
 
-exec('ls -la',(err,stdout,stderr)=>{
+child.stdout.on('data', function (data) {
+		console.log(`This is the stout of the command: ${data}`)
+})
+
+child.stderr.on('data',(data)=>{
+		console.error(`Child sdterr: ${data}`)
+})
+
+/*exec('ls -la',(err,stdout,stderr)=>{
 	if(err){
 		console.log(err)
 		return false;
 	}
 	console.log(stdout)
-})
+})*/
 
+/*Pipe Function- We can use it to send a command to a writable stream such as stouint*/
 
+const input=spawn('wc')
+
+process.stdin.pipe(input.stdin)
+
+/*input.stdout.on('data',(data)=>{
+       console.log(`Child sdtout using pipe: ${data}`)
+	 })*/
--- expres.js
+++ expres.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 
 app.use('/', function (req,res){
-	res.send('Hello World from Express')
+	res.sendFile(__dirname + '/html/index.html')
+	console.log("Hello")
 });
 
-app.listen(3000);
+app.listen(3001);
 
 console.log("App is listening on PORT 3000");
+console.log("This is your __dirname",__dirname)
